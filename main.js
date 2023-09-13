c2c2b861861e4cd791e54ad7dd0aa5e3e667d5b8
LLikely <lynn@likelyarts.com>
2023-09-13 12:48:36 -0700

    Create project

---
+++ main.js
@@ -0,0 +1,48 @@
+/* Exercise 1 
+1. In index.html, link to this file using the script tag.
+2. Log the document in the console.
+*/
+
+
+/* Exercise 2
+1. Create a variable pageTitle
+Set its value to the method getElementById. Pass the method the id of the h1 tag. 
+2. Log pageTitle in the console
+3. Create a variable pageTitle2
+Set its value to the method querySelector. Pass the method the id of the h1 tag. Don't forget id syntax!
+4. Log pageTitle2 in the console
+*/
+
+
+
+
+/* Exercise 3
+1. Log the textContent of the pageTitle in the console
+2. Change text content of the h1 tag to "Moon Facts"
+3. Create a variable fact1
+Give it a value of a string, pick one of the facts from moon-facts.txt
+4. Use the DOM to select the p tag inside the first div
+5. Set its textContent to fact1
+*/
+
+
+
+
+
+/* Exercise 4
+1. Use the DOM to select the div with id moon-fact-2
+2. Set its innerHTML to a p tag with class fact and containing the text of another fact from moon-facts.txt
+Hint - remember to escape the quote marks inside the p tag.
+3. Use an assignment operator you learned in the last lesson so the flag doesn't disappear.
+4. Do the same thing for moon-fact-3, moon-fact-4, moon-fact-5
+*/
+
+
+
+
+/* Exercise 5
+1. Use the DOM to select the image tag with id "secret"
+2. Use the src property to change the pin image to alien-ship.png
+3. Select the div tag and add a p tag to its innerHTML 
+Add some text inside the p tag about a secret alien moon base.
+*/
---
+++ practice-session/practice-session.js
@@ -0,0 +1,16 @@
+/* 
+Practice session
+Project:
+Open index.html with Live Server. There is a list of 6 statements.
+It's your job to assess whether they're true or not.
+Use JavaScript to add a comment under each statement that says one of the following 3 things:
+"That is true, I looked it up."
+"That is not true at all."
+"That is the silliest thing I've ever heard!"
+
+Look at the example screenshot what a commented statement looks like.
+Hint 1 - Examine the HTML of each statement so you can determine which tags to select.
+Hint 2 - Add a unigue id to each tag where you need to add JavaScript
+Hint 3 - Don't forget to use the script tag to link to this file.
+Hint 4 - Use variables to save yourself some repetitive typing.
+*/
